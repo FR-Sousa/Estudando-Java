151378ca0554e6f90a43a0d43d12267b5b26525a
FR-Sousa <filiperafael71@outlook.com>
2025-02-22 18:45:28 -0300

    Update OlaMundo.java

--- ExeciciosBasicos/OlaMundo.java
+++ ExeciciosBasicos/OlaMundo.java
@@ -1,5 +1,9 @@
 package ExeciciosBasicos;
 
+/*
+*  Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
+* */
+
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.print("Olá Mundo");
